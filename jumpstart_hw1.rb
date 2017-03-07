16c55274b43a67187ad2b0f9789c1af3664833cd
Drew <drew.b.stew@gmail.com>
2017-03-06 22:40:22 -0800

    appa hw 1

---
+++ jumpstart_hw1.rb
@@ -0,0 +1,19 @@
+
+# Three Digit Format
+
+# Define a method, #three_digit_format(n), that accepts an integer, n, as an
+# argument. Assume that n < 1000. Your method should return a string version of
+# n, but with leading zeros such that the string is always 3 characters long.
+
+def three_digit_format(n)
+  n = n.to_s.split('')
+  until n.size == 3
+    n = n.unshift("0")
+  end
+  n.join
+end
+
+puts "------Three Digit Format------"
+puts three_digit_format(100) == "100"
+puts three_digit_format(15) == "015"
+puts three_digit_format(6) == "006"
